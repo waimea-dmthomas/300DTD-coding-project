0bacd380ab78dfb291d483aeb628c96ced1f281b
waimea-dmthomas <dmthomas@waimea.school.nz>
2024-10-17 10:55:31 +1300

    g

--- src/Main.kt
+++ src/Main.kt
@@ -35,6 +35,7 @@ class MenuGUI : JFrame(), ActionListener {
     private lateinit var titleLabel: JLabel
     private lateinit var prepareButton: JButton
     private lateinit var startButton: JButton
+    private lateinit var exitPrepareButton: JButton
 
     /**
      * Create, build and run the UI
@@ -53,7 +54,7 @@ class MenuGUI : JFrame(), ActionListener {
      */
     private fun setupWindow() {
         title = "Caves, Creatures and Carrots"
-        contentPane.preferredSize = Dimension(300, 300)
+        contentPane.preferredSize = Dimension(500, 500)
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
         layout = null
@@ -68,21 +69,30 @@ class MenuGUI : JFrame(), ActionListener {
         val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 20)
 
         titleLabel = JLabel("Caves, Creatures and Carrots", SwingConstants.CENTER)
-        titleLabel.bounds = Rectangle(30, 30, 240, 40)
+        titleLabel.bounds = Rectangle(76, 62, 348, 36)
         titleLabel.font = baseFont
         add(titleLabel)
 
         prepareButton = JButton("Prepare")
-        prepareButton.bounds = Rectangle(30,130,240,40)
+        prepareButton.bounds = Rectangle(130,309,240,40)
         prepareButton.font = baseFont
         prepareButton.addActionListener(this)
         add(prepareButton)
 
         startButton = JButton("Enter the Cave")
-        startButton.bounds = Rectangle(30,186,240,40)
+        startButton.bounds = Rectangle(130,374,240,40)
         startButton.font = baseFont
         startButton.addActionListener(this)
         add(startButton)
+
+        exitPrepareButton = JButton("Exit")
+        exitPrepareButton.bounds = Rectangle(130,374,240,40)
+        exitPrepareButton.font = baseFont
+        exitPrepareButton.addActionListener(this)
+        exitPrepareButton.isVisible = false
+        add(exitPrepareButton)
+
+
     }
 
     /**
@@ -92,6 +102,7 @@ class MenuGUI : JFrame(), ActionListener {
         when (e?.source) {
             prepareButton -> prepareAction()
             startButton -> startAction()
+            exitPrepareButton -> exitPrepareAction()
         }
     }
 
@@ -99,64 +110,22 @@ class MenuGUI : JFrame(), ActionListener {
      * An Example Action
      */
     private fun prepareAction() {
-        startButton.isEnabled = false
-        SetupGUI()
+        startButton.isVisible = false
+        prepareButton.isVisible = false
+        titleLabel.isVisible = false
+        exitPrepareButton.isVisible = true
     }
 
     private fun startAction() {
-        titleLabel.text = "You Clicked!"
-    }
-}
-
-//=============================================================================================
-
-class SetupGUI : JFrame(), ActionListener {
-    // Setup some properties to hold the UI elements
-    private lateinit var exitButton: JButton
-
-    /**
-     * Create, build and run the UI
-     */
-    init {
-        setupWindow()
-        buildUI()
-
-        // Show the app, centred on screen
-        setLocationRelativeTo(null)
-        isVisible = true
+        startButton.isVisible = false
+        prepareButton.isVisible = false
+        titleLabel.isVisible = false
     }
 
-    private fun setupWindow() {
-        title = "Setup"
-        contentPane.preferredSize = Dimension(500, 500)
-        defaultCloseOperation = WindowConstants.HIDE_ON_CLOSE
-        isResizable = false
-        layout = null
-
-        pack()
-    }
-
-    private fun buildUI() {
-        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 20)
-
-        exitButton = JButton("Exit")
-        exitButton.bounds = Rectangle(30,130,240,40)
-        exitButton.font = baseFont
-        exitButton.addActionListener(this)
-        add(exitButton)
-    }
-
-    override fun actionPerformed(e: ActionEvent?) {
-        when (e?.source) {
-            exitButton -> exitAction()
-        }
-    }
-
-    /**
-     * An Example Action
-     */
-    private fun exitAction() {
-
+    private fun exitPrepareAction() {
+        startButton.isVisible = true
+        prepareButton.isVisible = true
+        titleLabel.isVisible = true
     }
 }
 
