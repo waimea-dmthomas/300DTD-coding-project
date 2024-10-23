b0288f7c614693a6b7201271462cd5bff2ab1b4c
waimea-dmthomas <dmthomas@waimea.school.nz>
2024-10-24 10:50:47 +1300

    s

--- src/Main.kt
+++ src/Main.kt
@@ -21,7 +21,40 @@ import javax.swing.*
 
 //=============================================================================================
 
-class Location(val type: String)
+class Location(val name: String, val desc: String) {
+    var north: Location? = null
+    var east: Location? = null
+    var south: Location? = null
+    var west: Location? = null
+
+    fun addNorth(location: Location) {
+        if (north == null) {
+            north = location
+            location.addSouth(this)
+        }
+    }
+
+    fun addEast(location: Location) {
+        if (east == null) {
+            east = location
+            location.addWest(this)
+        }
+    }
+
+    fun addSouth(location: Location) {
+        if (south == null) {
+            south = location
+            location.addEast(this)
+        }
+    }
+
+    fun addWest(location: Location) {
+        if (west == null) {
+            west = location
+            location.addNorth(this)
+        }
+    }
+}
 
 //=============================================================================================
 
@@ -31,22 +64,47 @@ class Location(val type: String)
  */
 class MenuGUI : JFrame(), ActionListener {
 
+    // Setup list of locations
+    val locations = mutableListOf<Location>()
+    var currentLocation: Location
+
     // Setup some properties to hold the UI elements
     private lateinit var titleLabel: JLabel
+
     private lateinit var prepareButton: JButton
     private lateinit var startButton: JButton
     private lateinit var exitPrepareButton: JButton
+
+    private lateinit var northButton: JButton
+    private lateinit var eastButton: JButton
+    private lateinit var southButton: JButton
+    private lateinit var westButton: JButton
+
+    private lateinit var statusHeaderLabel: JLabel
+    private lateinit var statusLabel: JLabel
+
+    private lateinit var locationHeaderLabel: JLabel
+    private lateinit var locationLabel: JLabel
 
     /**
      * Create, build and run the UI
      */
     init {
         setupWindow()
+        setupMap()
         buildUI()
+
+        currentLocation = locations.first()
 
         // Show the app, centred on screen
         setLocationRelativeTo(null)
         isVisible = true
+    }
+
+    private fun setupMap() {
+        val surface = Location("Surface", "The opening of a large cave.")
+
+        locations.add(surface)
     }
 
     /**
@@ -92,7 +150,57 @@ class MenuGUI : JFrame(), ActionListener {
         exitPrepareButton.isVisible = false
         add(exitPrepareButton)
 
+        northButton = JButton("^")
+        northButton.bounds = Rectangle(225,351,50,50)
+        northButton.font = baseFont
+        northButton.addActionListener(this)
+        northButton.isVisible = false
+        add(northButton)
 
+        eastButton = JButton(">")
+        eastButton.bounds = Rectangle(284,380,50,50)
+        eastButton.font = baseFont
+        eastButton.addActionListener(this)
+        eastButton.isVisible = false
+        add(eastButton)
+
+        southButton = JButton("v")
+        southButton.bounds = Rectangle(225,411,50,50)
+        southButton.font = baseFont
+        southButton.addActionListener(this)
+        southButton.isVisible = false
+        add(southButton)
+
+        westButton = JButton("<")
+        westButton.bounds = Rectangle(166,380,50,50)
+        westButton.font = baseFont
+        westButton.addActionListener(this)
+        westButton.isVisible = false
+        add(westButton)
+
+        statusHeaderLabel = JLabel("STATUS", SwingConstants.CENTER)
+        statusHeaderLabel.bounds = Rectangle(119, 278, 262, 20)
+        statusHeaderLabel.font = baseFont
+        statusHeaderLabel.isVisible = false
+        add(statusHeaderLabel)
+
+        statusLabel = JLabel("", SwingConstants.CENTER)
+        statusLabel.bounds = Rectangle(119, 298, 262, 20)
+        statusLabel.font = baseFont
+        statusLabel.isVisible = false
+        add(statusLabel)
+
+        locationHeaderLabel = JLabel("LOCATION", SwingConstants.CENTER)
+        locationHeaderLabel.bounds = Rectangle(119, 236, 262, 20)
+        locationHeaderLabel.font = baseFont
+        locationHeaderLabel.isVisible = false
+        add(locationHeaderLabel)
+
+        locationLabel = JLabel("", SwingConstants.CENTER)
+        locationLabel.bounds = Rectangle(119, 254, 262, 20)
+        locationLabel.font = baseFont
+        locationLabel.isVisible = false
+        add(locationLabel)
     }
 
     /**
@@ -103,11 +211,16 @@ class MenuGUI : JFrame(), ActionListener {
             prepareButton -> prepareAction()
             startButton -> startAction()
             exitPrepareButton -> exitPrepareAction()
+
+            northButton -> goNorth()
+            eastButton -> goEast()
+            southButton -> goSouth()
+            westButton -> goWest()
         }
     }
 
     /**
-     * An Example Action
+     * Functions
      */
     private fun prepareAction() {
         startButton.isVisible = false
@@ -120,12 +233,85 @@ class MenuGUI : JFrame(), ActionListener {
         startButton.isVisible = false
         prepareButton.isVisible = false
         titleLabel.isVisible = false
+
+        northButton.isVisible = true
+        eastButton.isVisible = true
+        southButton.isVisible = true
+        westButton.isVisible = true
+
+        statusHeaderLabel.isVisible = true
+        statusLabel.isVisible = true
+        locationHeaderLabel.isVisible = true
+        locationLabel.isVisible = true
+
+        showLocation()
     }
 
     private fun exitPrepareAction() {
         startButton.isVisible = true
         prepareButton.isVisible = true
         titleLabel.isVisible = true
+    }
+
+    private fun showLocation() {
+        locationLabel.text = currentLocation.name
+        statusLabel.setText("<html>" + currentLocation.desc + "</html>")
+        checkNorth()
+        checkEast()
+        checkSouth()
+        checkWest()
+    }
+
+    private fun checkNorth() {
+        if(currentLocation.north != null) {
+            northButton.isEnabled = true
+        }
+        else {
+            northButton.isEnabled = false
+        }
+    }
+
+    private fun checkEast() {
+        if(currentLocation.east != null) {
+            eastButton.isEnabled = true
+        }
+        else {
+            eastButton.isEnabled = false
+        }
+    }
+
+    private fun checkSouth() {
+        if(currentLocation.south != null) {
+            southButton.isEnabled = true
+        }
+        else {
+            southButton.isEnabled = false
+        }
+    }
+
+    private fun checkWest() {
+        if(currentLocation.west != null) {
+            westButton.isEnabled = true
+        }
+        else {
+            westButton.isEnabled = false
+        }
+    }
+
+    private fun goNorth() {
+
+    }
+
+    private fun goEast() {
+
+    }
+
+    private fun goSouth() {
+
+    }
+
+    private fun goWest() {
+
     }
 }
 
